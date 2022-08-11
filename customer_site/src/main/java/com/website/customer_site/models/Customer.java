e8f02f2f5cc299730be8ff21aeed886fc7a59147
jparsons153 <jparsons153@hotmail.com>
2022-08-11 10:29:57 +0100

    new entity RentalCars created mapped OneToOne to customers

--- customer_site/src/main/java/com/website/customer_site/models/Customer.java
+++ customer_site/src/main/java/com/website/customer_site/models/Customer.java
@@ -20,4 +20,7 @@ public class Customer {
     private String emailAddress;
     private Integer age;
     private String address;
+
+    @OneToOne
+    RentalCar rentals;
 }
---
+++ customer_site/src/main/java/com/website/customer_site/models/RentalCar.java
@@ -0,0 +1,27 @@
+package com.website.customer_site.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "rentalcars")
+@Builder
+@Getter
+@Setter
+
+public class RentalCar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String make;
+    private String model;
+    private String registration;
+
+    // bi-directional or uni directional?
+//    @OneToOne
+//    mappedBy = "rentals"
+//    Customer customer;
+}
